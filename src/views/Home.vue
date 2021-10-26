3ff40160f11200f769cc8b58ea2478dd5cf8cbf3
AdRuslan <russian.adig@mail.ru>
2021-10-27 02:13:09 +0300

    add carousel in Home component

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,12 +1,32 @@
 <template>
-  <h1>Главная страница</h1>
+  <v-container>
+    <h1>Главная страница</h1>
+    <v-carousel>
+      <v-carousel-item
+        v-for="(item, i) in articles"
+        :key="i"
+        :src="'/img/' + item.full_image"
+        reverse-transition="fade-transition"
+        transition="fade-transition"
+      ></v-carousel-item>
+    </v-carousel>
+  </v-container>
 </template>
 
 <script>
-
 export default {
   name: "Home",
 
-  components: {},
+  computed: {
+    articles() {
+      return this.$store.getters.GET_ARTICLES.filter(
+        (item) => item.slider === true
+      );
+    },
+  },
+
+  created() {
+    this.$store.dispatch("GET_ARTICLES");
+  },
 };
 </script>
